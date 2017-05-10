bb9e0151abe784095d695654a7c25367cf849bac
Jan Skrasek <hrach.cz@gmail.com>
2017-05-10 21:48:02 +0200

    fixed caching styles in browser

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -8,4 +8,11 @@ use Nette\Application\UI\Presenter;
 abstract class BasePresenter extends Presenter
 {
 	public $invalidLinkMode = self::INVALID_LINK_EXCEPTION;
+
+
+	protected function beforeRender()
+	{
+		$this->template->appDir = __DIR__ . '/../';
+		parent::beforeRender();
+	}
 }
