8ee9f42a1ccd548ba2c7fbe13e0cd0645e947bc4
Jan Skrasek <hrach.cz@gmail.com>
2021-05-23 00:54:52 +0200

    parse doc and reuse first heading and page title

--- app/DocsModule/presenters/BrowserPresenter.php
+++ app/DocsModule/presenters/BrowserPresenter.php
@@ -2,10 +2,13 @@
 
 namespace Nextras\Web\Docs;
 
+use League\CommonMark\MarkdownConverter;
+use League\CommonMark\Node\Inline\Text;
 use Nette\Application\Attributes\Persistent;
 use Nette\Application\Responses\FileResponse;
 use Nette\DI\Attributes\Inject;
 use Nette\DI\Container;
+use Nette\NotImplementedException;
 use Nextras\Web\BasePresenter;
 
 
@@ -52,9 +55,41 @@ class BrowserPresenter extends BasePresenter
 
 	public function renderDefault()
 	{
+		$menu = $this->documentationService->get($this->component, $this->version, "menu");
+
+		[$content, $title] = $this->parse($this->page);
+		[$menuContent] = $menu !== null ? $this->parse($menu) : [null];
+
 		$this->template->container = $this->container;
 		$this->template->chapter = $this->page;
-		$this->template->menu = $this->documentationService->get($this->component, $this->version, "menu");
+		$this->template->title = $title;
+		$this->template->content = $content;
+		$this->template->menuContent = $menuContent;
 		$this->template->versions = $this->documentationService->getVersions($this->component);
+	}
+
+
+	/**
+	 * @return array<string>
+	 */
+	private function parse(TextPage $page): array
+	{
+		if ($page->getType()->is(ContentType::TEXY)) {
+			$texy = $this->container->getByType(\Texy::class);
+			$output = $texy->process($page->getContent());
+			return [$output, $texy->headingModule->title];
+		} elseif ($page->getType()->is(ContentType::MARKDOWN)) {
+			$converter = $this->container->getByType(MarkdownConverter::class);
+			$parsed = $converter->convertToHtml($page->getContent());
+			$heading = $parsed->getDocument()->firstChild()?->firstChild();
+			if ($heading instanceof Text) {
+				$title = $heading->getLiteral();
+			} else {
+				$title = "";
+			}
+			return [$parsed->getContent(), $title];
+		} else {
+			throw new NotImplementedException();
+		}
 	}
 }
--- app/DocsModule/presenters/BrowserTemplate.php
+++ app/DocsModule/presenters/BrowserTemplate.php
@@ -2,15 +2,10 @@
 
 namespace Nextras\Web\Docs;
 
-use Latte\Attributes\TemplateFilter;
-use Latte\Engine;
-use Latte\Runtime\FilterInfo;
-use League\CommonMark\MarkdownConverter;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Presenter;
 use Nette\Bridges\ApplicationLatte\Template;
 use Nette\DI\Container;
-use Nette\NotImplementedException;
 
 
 class BrowserTemplate extends Template
@@ -18,8 +13,6 @@ class BrowserTemplate extends Template
 	public Container $container;
 
 	public TextPage $chapter;
-
-	public ?TextPage $menu;
 
 	/** @var array<string> */
 	public array $versions;
@@ -32,27 +25,12 @@ class BrowserTemplate extends Template
 
 	public string $basePath;
 
+	public string $title;
+
+	public string $content;
+
+	public ?string $menuContent;
+
 	/** @var array<\stdClass> */
 	public array $flashes = [];
-
-
-	#[TemplateFilter]
-	function parse(
-		FilterInfo $info,
-		string $content,
-		ContentType $contentType,
-	): string
-	{
-		$info->contentType = Engine::CONTENT_HTML;
-
-		if ($contentType->is(ContentType::TEXY)) {
-			$texy = $this->container->getByType(\Texy::class);
-			return $texy->process($content);
-		} elseif ($contentType->is(ContentType::MARKDOWN)) {
-			$converter = $this->container->getByType(MarkdownConverter::class);
-			return $converter->convertToHtml($content)->getContent();
-		} else {
-			throw new NotImplementedException();
-		}
-	}
 }
